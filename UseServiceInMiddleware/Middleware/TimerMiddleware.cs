6667e65212a22f8c4ade35b5f61ee159840697f9
Zorrys9 <saschek2000@yandex.ru>
2020-01-23 18:54:10 +0300

    use service in middleware

---
+++ UseServiceInMiddleware/Middleware/TimerMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UseServiceInMiddleware.Services;
+using Microsoft.Extensions.DependencyInjection;
+namespace UseServiceInMiddleware.Middleware
+{
+    public class TimerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public TimerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context, TimerService timerService)
+        {
+            if(context.Request.Path.Value.ToLower() == "/time")
+            {
+                context.Response.ContentType = "text/html;charset=utf-8";
+                //TimerService timer = context.RequestServices.GetService<TimerService>();
+                await context.Response.WriteAsync($"Текущее время: {timerService?.Time}");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ UseServiceInMiddleware/Services/TimerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UseServiceInMiddleware.Services
+{
+    public class TimerService
+    {
+        public string Time { get; }
+        public TimerService()
+        {
+            Time = System.DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
